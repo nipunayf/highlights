35af392517b02d07fba540ed5421d64eb3872b4a
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-06-16 23:18:56 +0530

    Automatically set account

--- src/website/highlights/src/pages/_app.tsx
+++ src/website/highlights/src/pages/_app.tsx
@@ -3,11 +3,33 @@ import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
 import { createTheme, MantineProvider } from '@mantine/core';
 import { MsalProvider } from '@azure/msal-react';
-import { PublicClientApplication } from '@azure/msal-browser';
+import { AuthenticationResult, EventType, PublicClientApplication } from '@azure/msal-browser';
 import { msalConfig } from '../authConfig';
 import PageLayout from '@/components/PageLayout';
 
 const msalInstance = new PublicClientApplication(msalConfig);
+
+if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+    // Account selection logic is app dependent. Adjust as needed for different use cases.
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+}
+
+// Optional - This will update account state if a user signs in from another tab or window
+msalInstance.enableAccountStorageEvents();
+
+msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS
+        ||
+        event.eventType === EventType.ACQUIRE_TOKEN_SUCCESS
+        ||
+        event.eventType === EventType.SSO_SILENT_SUCCESS
+    ) {
+        const account = (event.payload as AuthenticationResult)?.account;
+        if (account) {
+            msalInstance.setActiveAccount(account);
+        }
+    }
+});
 
 const theme = createTheme({
     /** Put your mantine theme override here */

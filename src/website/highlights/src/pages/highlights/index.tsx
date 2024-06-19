065cd28085bd039be4f52afc9cbc0ec1a8958286
Imalkaaa <imalkadhananja28@gmail.com>
2024-06-20 00:04:37 +0530

    create the card

--- src/website/highlights/src/pages/highlights/index.tsx
+++ src/website/highlights/src/pages/highlights/index.tsx
@@ -1,19 +1,45 @@
 import PageLayout from "@/components/PageLayout";
-import { Title } from "@mantine/core";
+import { Title, Card, Text, SimpleGrid, UnstyledButton, Anchor, Group, useMantineTheme } from "@mantine/core";
 import { ReactNode } from "react";
+import classes from "./ActionsGrid.module.css";
+
+
+
+function ActionsGrid() {
+  const theme = useMantineTheme();
+
+  
+  return (
+    <Card withBorder radius="10px"  className={classes.card}>
+      <Group >
+      <img
+          src="/add-plus-svgrepo-com (1).svg"
+          alt="Add Task Icon"
+          className={classes.icon}
+        />
+        <Text className={classes.title}>Add Highlights</Text> 
+        
+       
+        
+        </Group>
+      
+    </Card>
+  );
+}
 
 export default function Highlights() {
-    return (
-        <>
-            <Title order={1}>Highlights</Title>
-        </>
-    )
+  return (
+    <>
+      {/* <Title order={1}>Highlights</Title> */}
+      <ActionsGrid />
+    </>
+  );
 }
 
 Highlights.getLayout = function getLayout(page: ReactNode) {
-    return (
-        <PageLayout>
-            {page}
-        </PageLayout>
-    );
+  return (
+    <PageLayout>
+      {page}
+    </PageLayout>
+  );
 }

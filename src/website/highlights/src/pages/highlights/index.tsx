93c3836fa7e53ac49f4bd049d47646cfb4453679
Imalkaaa <imalkadhananja28@gmail.com>
2024-07-08 16:20:02 +0530

    optionn menu convert in to mantine

--- src/website/highlights/src/pages/highlights/index.tsx
+++ src/website/highlights/src/pages/highlights/index.tsx
@@ -1,9 +1,8 @@
 import React, { ReactNode, useState } from 'react';
 import { Card, Group, Text, useMantineTheme } from '@mantine/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSquare as faRegularSquare, faCheckSquare as faSolidSquare } from '@fortawesome/free-solid-svg-icons';
 import { faSquare as faRegularSquare } from '@fortawesome/free-regular-svg-icons';
-import { faCheckSquare as faSolidSquare } from '@fortawesome/free-solid-svg-icons';
+// import { faCheckSquare as faSolidSquare } from '@fortawesome/free-solid-svg-icons';
 
 import Confetti from 'react-confetti';
 import PageLayout from "@/components/PageLayout";

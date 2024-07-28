403ba7465252a069046c248426d23b5f57ed58b6
DiliniUdawaththa <dmadhushani611@gmail.com>
2024-07-28 13:15:53 +0530

    Links added to Navbar and Footer

--- src/landingpage/src/components/navbar/Navbar.tsx
+++ src/landingpage/src/components/navbar/Navbar.tsx
@@ -8,6 +8,7 @@ import { IoMenu } from 'react-icons/io5';
 import { IoMdClose } from 'react-icons/io';
 import { useState } from 'react';
 import Logo from "../../assests/logo.png";
+import { webAppURL } from "@/config";
 
 const Navbar = () => {
     const [showNav, setShowNav] = useState(false);
@@ -15,37 +16,40 @@ const Navbar = () => {
     return (
         <header className="navbar">
             <nav className="navbar_container wrapper">
-                
+
+                <div className="logo_container">
                     <a href="#" className="navbar_logo" onClick={() => setShowNav(false)}>
-                        <Image 
-                            src={Logo} 
-                            alt="logo" 
+                        <Image
+                            src={Logo}
+                            alt="logo"
                             priority
                         />
                     </a>
+                    <h3>Highlights</h3>
+                </div>
 
-                    <ul className={`${showNav ? "show" : ""}`}>
-                        <li onClick={() => setShowNav(false)}>
-                            <Link href="/home"> Home </Link>
-                        </li>
-                        <li onClick={() => setShowNav(false)}>
-                            <Link href="/features"> Features </Link>
-                        </li>
-                        <li onClick={() => setShowNav(false)}>
-                            <Link href="/help"> Help </Link>
-                        </li>
-                    </ul>
+                <ul className={`${showNav ? "show" : ""}`}>
+                    <li onClick={() => setShowNav(false)}>
+                        <Link href="/home"> Home </Link>
+                    </li>
+                    <li onClick={() => setShowNav(false)}>
+                        <Link href="/features"> Features </Link>
+                    </li>
+                    <li onClick={() => setShowNav(false)}>
+                        <Link href="/help"> Help </Link>
+                    </li>
+                </ul>
 
-                    <div className="navbar_btns">
-                        <a href="#" className="btn">Login</a>
-                        <a href="#" className="btn">Sign Up</a>
-                    </div>
+                <div className="navbar_btns">
+                    <a href={webAppURL} className="btn">Login</a>
+                    <a href={webAppURL} className="btn">Sign Up</a>
+                </div>
 
-                    <div className="navbar_menu" onClick={() => setShowNav(!showNav)}>
-                        {showNav ? <IoMdClose /> : <IoMenu />}
-                    </div>
+                <div className="navbar_menu" onClick={() => setShowNav(!showNav)}>
+                    {showNav ? <IoMdClose /> : <IoMenu />}
+                </div>
 
-                
+
             </nav>
         </header>
     );

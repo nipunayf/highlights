439f38d8324c868d335c175f4774a7d31fcce48a
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-07-29 13:51:07 +0530

    Add authentication

--- src/admin-dashboard/src/pages/_app.tsx
+++ src/admin-dashboard/src/pages/_app.tsx
@@ -4,6 +4,31 @@ import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import { MsalAuthenticationTemplate, MsalProvider } from '@azure/msal-react';
+import { AuthenticationResult, EventType, InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from '../authConfig';
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+}
+
+msalInstance.enableAccountStorageEvents();
+
+msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS
+        ||
+        event.eventType === EventType.ACQUIRE_TOKEN_SUCCESS
+        ||
+        event.eventType === EventType.SSO_SILENT_SUCCESS
+    ) {
+        const account = (event.payload as AuthenticationResult)?.account;
+        if (account) {
+            msalInstance.setActiveAccount(account);
+        }
+    }
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -17,9 +42,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [pathname, router]);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <MsalProvider instance={msalInstance}>
+      <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MsalAuthenticationTemplate>
+    </MsalProvider>
   );
 }
 

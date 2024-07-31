b57e727133030afebe5380647feec521ba472ead
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-07-31 15:45:50 +0530

    Add samaple data

--- src/database/scripts/seed-data.sql
+++ src/database/scripts/seed-data.sql
@@ -1,4 +1,6 @@
+insert into users values(1, '6e4096d8-b99e-4b2d-a495-446cce5fcd7c');
 
+insert into task_lists(user_id, title) values(1, 'Shopping'),(1, 'Grocery'),(1, 'Work'),(1, 'Travel');
 
 INSERT INTO `timer_details` (
   `timer_name`,
@@ -19,9 +21,6 @@ INSERT INTO `timer_details` (
   ('Meditation', '00:20:00', '00:05:00', '00:15:00', 3, 5),
   ('Writing Session', '00:45:00', '00:08:00', '00:30:00', 4, 5);
 
-
-
-
 INSERT INTO `hilights_hasintha` (`highlight_id`, `highlight_name`, `user_id`)
 VALUES 
 (1, 'Introduction to Ballerina', 101),
@@ -35,10 +34,6 @@ VALUES
 (9, 'Ballerina Integration with Other Systems', 109),
 (10, 'Advanced Ballerina Features', 110);
 
-insert into users values(1, '6e4096d8-b99e-4b2d-a495-446cce5fcd7c');
-
-insert into task_lists(user_id, title) values(1, 'Shopping'),(1, 'Grocery'),(1, 'Work'),(1, 'Travel');
-
 INSERT INTO highlights.PausesPomoDetails (pauses_pomo_id, highlight_id, pause_time, continue_time)
 VALUES 
 (1, 9, '2024-07-31 11:27:07', '2024-07-31 11:27:08'),
@@ -46,8 +41,12 @@ VALUES
 (3, 9, '2024-07-31 11:27:12', '2024-07-31 11:27:14'),
 (4, 7, '2024-07-31 11:37:18', '2024-07-31 11:37:20');
 
-
 INSERT INTO highlights.HighlightPomoDetails (timer_id, highlight_id, user_id, start_time, end_time, status)
 VALUES (10, 9, 11, '2024-07-31 11:27:14', '2024-07-31 11:27:17', 'complete'),
 (8, 7, 11, '2024-07-31 11:37:23', '2024-07-31 11:37:26', 'complete');
 
+INSERT INTO highlights.hi (id, title, dueDate, startTime, endTime, label, reminder, priority, description, status)
+VALUES (1, 'Buy a present', '2024-08-01', '12:00:00', '13:00:00', 'Shopping', 'Before 30 minutes', 'medium', 'Description', 'pending'),
+       (2, 'Read emails', '2024-08-01', '08:00:00', '09:00:00', 'Reading', 'Before 10 minutes', 'high', 'Description', 'pending'),
+       (3, 'Buy milk', '2024-08-01', '13:00:00', '14:00:00', 'Shopping', 'Before 10 minutes', 'high', 'Description', 'pending'),
+       (4, 'Read a book', '2024-08-01', '20:00:00', '22:00:00', 'Reading', 'Before 10 minutes', 'high', 'Description', 'pending');

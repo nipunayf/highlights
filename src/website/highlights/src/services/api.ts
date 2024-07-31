9f3ebba72a2a5665cca4bcfebc51b6566cb8f3ab
jagath <jagathdammika17595@gmail.com>
2024-07-31 19:19:46 +0530

    add new functions

--- src/website/highlights/src/services/api.ts
+++ src/website/highlights/src/services/api.ts
@@ -12,25 +12,25 @@ function getAxiosClient(route: string): AxiosInstance {
         baseURL: `${apiEndpoint}/${route}`
     });
 
-    client.interceptors.request.use(async (config) => {
-        config.headers['Authorization'] = `Bearer ${await aquireAccessToken()}`;
-        return config;
-    }, (error) => {
-        return Promise.reject(error);
-    });
+    // client.interceptors.request.use(async (config) => {
+    //     config.headers['Authorization'] = `Bearer ${await aquireAccessToken()}`;
+    //     return config;
+    // }, (error) => {
+    //     return Promise.reject(error);
+    // });
 
     return client;
 }
 
-export async function getTaskLists(user: AppUser) {
-    const response = await getAxiosClient('taskLists').request({
-        method: 'GET',
-        params: {
-            sub: user.sub
-        }
-    });
-    return response.data;
-}
+// export async function getTaskLists(user: AppUser) {
+//     const response = await getAxiosClient('taskLists').request({
+//         method: 'GET',
+//         params: {
+//             sub: user.sub
+//         }
+//     });
+//     return response.data;
+// }
 
 export async function getTasks(): Promise<Task[]> {
     const response = await getAxiosClient('tasks').request<Task[]>({
@@ -64,5 +64,76 @@ export async function addTip(tip: Tip): Promise<Tip> {
         data: tip
     });
     // console.log("Hello");
+    return response.data;
+}
+export async function getProjects() {
+    const response = await getAxiosClient('projects').request({
+        method: 'GET'
+    });
+
+    return response;
+}
+export async function addProjects(tip: any){
+    console.log("cc")
+    const response = await getAxiosClient('addProjects')({
+        method: 'POST',
+        data: tip
+    });
+    // console.log("Hello");
+    return response.data;
+}
+export async function updateProject(row: any){
+    // console.log("cc")
+    const response = await getAxiosClient('updateProject')({
+        method: 'PUT',
+        data: row
+    });
+    // console.log("Hello");
+    return response.data;
+}
+export async function getProjectDetails() {
+    const response = await getAxiosClient('project-details').request({
+        method: 'GET'
+    });
+
+    return response;
+}
+export async function addTask(row: any){
+    // console.log("cc")
+    const response = await getAxiosClient('addTask')({
+        method: 'POST',
+        data: row
+    });
+    // console.log(response);
+    // console.log("Hello");
+    return response.data;
+}
+export async function updateTask(row: any){
+    // console.log("cc")
+    const response = await getAxiosClient('updateTask')({
+        method: 'PUT',
+        data: row
+    });
+    // console.log("Hello");
+    return response.data;
+}
+export async function tasks(projectId:any) {
+    const response = await getAxiosClient(`tasks/${projectId}`).request({
+        method: 'GET'
+        // params: {
+        //     projectId: projectId
+        // }
+    });
+    console.log("jagaht");
+    return response.data;
+}
+export async function project(projectId:any) {
+    const response = await getAxiosClient(`project/${projectId}`).request({
+        method: 'GET'
+        // params: {
+        //     projectId: projectId
+        // }
+    });
+    // console.log(response);
     return response.data;
 }

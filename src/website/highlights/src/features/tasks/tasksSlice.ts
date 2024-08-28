c34557d1c792139091fb2670b6fc905f9e3a0372
Ashen Sandaruwan <59907222+ashen92@users.noreply.github.com>
2024-08-28 09:33:15 +0530

    Fix build errors related to tasks

--- src/website/highlights/src/features/tasks/tasksSlice.ts
+++ src/website/highlights/src/features/tasks/tasksSlice.ts
@@ -1,32 +1,32 @@
-import { Task } from '@/models/Task';
 import { RootState } from '@/store';
 import { createAsyncThunk, createEntityAdapter, createSlice, EntityState, PayloadAction } from '@reduxjs/toolkit';
 import { TaskList } from '../taskLists/TaskList';
 import { TaskListSource } from '../taskLists/TaskListSource';
 import { getTasks as getMSToDoTasks } from '@/services/GraphService';
 import { updateTaskListWithTasks } from '../taskLists/taskListsSlice';
+import { Task } from './models';
 
-const defaultState = [
-    { id: 'task1', title: 'Finish project proposal', dueDate: new Date('2024-07-25').toISOString(), created: new Date().toISOString() },
-    { id: 'task2', title: 'Call the client for feedback', dueDate: new Date('2024-07-22').toISOString(), created: new Date().toISOString() },
-    { id: 'task3', title: 'Prepare presentation for Monday\'s meeting', dueDate: new Date('2024-07-24').toISOString(), created: new Date().toISOString() },
-    { id: 'task4', title: 'Submit quarterly report', dueDate: new Date('2024-07-29').toISOString(), created: new Date().toISOString() },
-    { id: 'task5', title: 'Update website content', dueDate: new Date('2024-07-26').toISOString(), created: new Date().toISOString() },
-    { id: 'task6', title: 'Organize team-building event', dueDate: new Date('2024-08-01').toISOString(), created: new Date().toISOString() },
-    { id: 'task7', title: 'Schedule performance reviews', dueDate: new Date('2024-07-28').toISOString(), created: new Date().toISOString() },
-    { id: 'task8', title: 'Backup all project files', dueDate: new Date('2024-07-30').toISOString(), created: new Date().toISOString() },
-    { id: 'task9', title: 'Review marketing strategy', dueDate: new Date('2024-07-23').toISOString(), created: new Date().toISOString() },
-    { id: 'task10', title: 'Plan next sprint', dueDate: new Date('2024-07-27').toISOString(), created: new Date().toISOString() },
-    { id: 'task11', title: 'Write blog post', dueDate: new Date('2024-07-21').toISOString(), created: new Date().toISOString() },
-    { id: 'task12', title: 'Update CRM system', dueDate: new Date('2024-07-31').toISOString(), created: new Date().toISOString() },
-    { id: 'task13', title: 'Send invoices to clients', dueDate: new Date('2024-07-20').toISOString(), created: new Date().toISOString() },
-    { id: 'task14', title: 'Conduct user interviews', dueDate: new Date('2024-08-02').toISOString(), created: new Date().toISOString() },
-    { id: 'task15', title: 'Fix bugs from the latest release', dueDate: new Date('2024-07-22').toISOString(), created: new Date().toISOString() },
-    { id: 'task16', title: 'Analyze sales data', dueDate: new Date('2024-07-25').toISOString(), created: new Date().toISOString() },
-    { id: 'task17', title: 'Prepare financial statements', dueDate: new Date('2024-07-28').toISOString(), created: new Date().toISOString() },
-    { id: 'task18', title: 'Update social media profiles', dueDate: new Date('2024-07-24').toISOString(), created: new Date().toISOString() },
-    { id: 'task19', title: 'Order office supplies', dueDate: new Date('2024-07-26').toISOString(), created: new Date().toISOString() },
-    { id: 'task20', title: 'Review and merge pull requests', dueDate: new Date('2024-07-27').toISOString(), created: new Date().toISOString() }
+const defaultState: Task[] = [
+    { id: 'task1', title: 'Finish project proposal', dueDate: new Date('2024-07-25').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task2', title: 'Call the client for feedback', dueDate: new Date('2024-07-22').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task3', title: 'Prepare presentation for Monday\'s meeting', dueDate: new Date('2024-07-24').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task4', title: 'Submit quarterly report', dueDate: new Date('2024-07-29').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task5', title: 'Update website content', dueDate: new Date('2024-07-26').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task6', title: 'Organize team-building event', dueDate: new Date('2024-08-01').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task7', title: 'Schedule performance reviews', dueDate: new Date('2024-07-28').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task8', title: 'Backup all project files', dueDate: new Date('2024-07-30').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task9', title: 'Review marketing strategy', dueDate: new Date('2024-07-23').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task10', title: 'Plan next sprint', dueDate: new Date('2024-07-27').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task11', title: 'Write blog post', dueDate: new Date('2024-07-21').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task12', title: 'Update CRM system', dueDate: new Date('2024-07-31').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task13', title: 'Send invoices to clients', dueDate: new Date('2024-07-20').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task14', title: 'Conduct user interviews', dueDate: new Date('2024-08-02').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task15', title: 'Fix bugs from the latest release', dueDate: new Date('2024-07-22').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task16', title: 'Analyze sales data', dueDate: new Date('2024-07-25').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task17', title: 'Prepare financial statements', dueDate: new Date('2024-07-28').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task18', title: 'Update social media profiles', dueDate: new Date('2024-07-24').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task19', title: 'Order office supplies', dueDate: new Date('2024-07-26').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' },
+    { id: 'task20', title: 'Review and merge pull requests', dueDate: new Date('2024-07-27').toISOString(), created: new Date().toISOString(), status: 'pending', taskListId: '1' }
 ];
 
 const tasksAdapter = createEntityAdapter<Task>({
@@ -57,6 +57,7 @@ export const fetchTasks = createAsyncThunk(
                     title: t.title,
                     created: t.createdDateTime,
                     status: t.status,
+                    taskListId
                 });
             }
             dispatch(updateTaskListWithTasks({ taskListId, taskIds: tasks.map(task => task.id) }));
--- src/website/highlights/src/models/Task.ts
+++ src/website/highlights/src/models/Task.ts
@@ -1,22 +1,20 @@
 import { SetStateAction } from "react";
+import { Entity } from "./Entity";
 
-export interface Task {
-
-    estimatedTime?: SetStateAction<number | null>;
-    dueDate?: string;
-    Date?: string;
+export interface Task extends Entity {
+    estimatedTime: SetStateAction<number | null>;
+    completed: any;
+    dueDate: Date | null;
+    Date: string;
     title: string;
-    description?: string;
-    startTime?: string;
-    endTime?: string;
-    reminder?: string;
-    priority?: string;
-    label?: string;
-    status?: 'overdue' | 'pending' | 'completed';
-    taskId?: string;
-
-    id: string;
-    created: string;
+    description: string;
+    startTime: string;
+    endTime: string;
+    reminder: string;
+    priority: string;
+    label: string;
+    status: 'overdue' | 'pending' | 'completed';
+    taskId: string;
 }
 
 export interface Review {
